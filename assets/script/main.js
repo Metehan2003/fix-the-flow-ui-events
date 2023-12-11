0218d6895ce781689bb63150f5b6913560f4452f
Metehan2003 <144008559+Metehan2003@users.noreply.github.com>
2023-12-11 13:00:28 +0100

    .

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,44 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+// Eerste knop
+
+let frontend = document.querySelector(".frontend")
+frontend.addEventListener("click", function () {
+  frontend.classList.toggle("klikt")
+})
+
+//tweede knop
+
+let design = document.querySelector(".design")
+design.addEventListener("dblclick", function () {
+  design.classList.toggle("effect")
+  console.log('double click')
+})
+
+//derde knop
+
+let en = document.querySelector(".en")
+en.addEventListener("keydown", function () {
+  en.classList.toggle("kleur")
+})
+
+let dev = document.querySelector(".dev")
+dev.addEventListener("keydown", function (event) {
+  // Als ik m indrukte, toggle dan de kleur
+  if (event.key == 'm') {
+    dev.classList.toggle("kleur")
+  }
+
+})
+
+let sprint = document.querySelector(".sprint")
+sprint.addEventListener("blur", function () {
+  sprint.classList.toggle("kleuren")
+})
+
+let fix = document.querySelector(".fix")
+fix.addEventListener("wheel", function () {
+  fix.classList.toggle("fixen")
+})
